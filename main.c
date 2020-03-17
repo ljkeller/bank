6869bea4cace408c9ba5ddff50b9873bdf9df96b
ljkeller <kellerjlucas@gmail.com>
2020-03-17 10:54:15 -0500

    Modularizing code. Parses input and checks validity. Has errno support. Using more helper functions

--- main.c
+++ main.c
@@ -16,27 +16,36 @@ int read_command(char command[], char *params[]);
 
 int validate_input(char* p, long arg, int err);
 
+void imm_response(int request_id);
+
+int proc_trans();
+
+void write_file(FILE *fptr, char* params[], int argc);
+
 int main(int argc, char* argv[]) {
     char *p, *params[PARAM_SIZE], *command;
     char filename[PARAM_SIZE], buffer[BUFFER_SIZE];
-    int n_workers, n_accounts, running, ret, i;
+    int n_workers, n_accounts, running, ret, i, request_id, account_id;
     errno = 0;
     FILE *fptr;
     long ID = 0;
 
     //Program initialization and input checking
     if(argc != 4) {
+        errno = EINVAL;
         perror("It looks like there were insufficient inputs");
     }
 
     long arg = strtol(argv[1], &p, 10);
     if( validate_input(p, arg, errno) != 0) {
+        errno = EINVAL;
         perror("It looks like there was in invalid input");
     }
     n_workers = arg; //Number of worker threads
     
     arg = strtol(argv[2], &p, 10);
     if( validate_input(p, arg, errno) != 0) {
+        errno = EINVAL;
         perror("It looks like there was in invalid input.\n");
     } 
     n_accounts = arg;
@@ -46,6 +55,7 @@ int main(int argc, char* argv[]) {
     strcat(filename, ".txt");
     fptr = fopen(filename, "w+");
     if(fptr == NULL) {
+        errno = EINVAL;
         perror("Bad file handling\n");
         exit(1);
     }
@@ -55,20 +65,49 @@ int main(int argc, char* argv[]) {
             n_workers, n_accounts, filename);
 
 
+    request_id = 0;
     // Ready to run bank server and take requests
     while(running) {
         ret = initialize_accounts(n_accounts);
-        if(ret != 0) {
+        if(ret == 0) {
+            errno = EHOSTUNREACH;
             perror("Unsuccessful account initializations.\n");
         }
+        printf("Accepting user input: ");
         p = fgets(buffer, ARG_MAX, stdin);
         if(p == NULL){
+            errno = EINVAL;
             perror("Looks like there was problems taking input.\n");
         }
+        request_id++;
         ret = read_command(buffer, params);
         command = params[0];
-        printf("The command was \"%s\" and the argc was: %d\n", command, ret);
-        fprintf(fptr, "%s", command);
+        
+        if(strcmp(command, "END") == 0) {
+            break;
+        } else if(strcmp(command, "TRANS") == 0) {
+            imm_response(request_id);
+            proc_trans();
+        } else if(strcmp(command, "CHECK") == 0) {
+            if(ret == 1) {
+                errno = EINVAL;
+                perror("Missing <accountid>");
+                request_id--;
+                break;
+            } 
+            imm_response(request_id); 
+            //write_file(fptr, params, ret); //Writes command and params to file
+            account_id = strtol(params[1], &p, 10);
+            printf("The account id was %d\n", account_id);
+            ret = read_account(account_id);
+            fprintf(fptr, "%d BAL  %d", request_id, ret);
+        } else {
+            errno = EINVAL;
+            request_id--;
+            perror("Failure to provide valid input");
+        }
+        //printf("The command was \"%s\" and the argc was: %d\n", command, ret);
+        /*fprintf(fptr, "%s", command);
 
         //Append user input to file to record
         //TODO: ensure only doing this when no threads are
@@ -76,6 +115,7 @@ int main(int argc, char* argv[]) {
             fprintf(fptr, " %s", params[i]);
         }
         fprintf(fptr, "\n");
+        */
 
         break;
     }
@@ -114,4 +154,22 @@ int validate_input(char* p, long arg, int err) {
         return 1;
     } else if(arg < INT_MIN || arg > INT_MAX) {
         return 2;
-    } else { return 0; } }
+    } else { 
+        return 0; 
+    } 
+}
+
+void imm_response(int request_id){
+    printf("ID %d\n", request_id);
+}
+
+int proc_trans(){
+    return 0;
+}
+
+void write_file(FILE *fptr, char* params[], int argc){
+    int i = 0;
+    for(i = 0; i<argc; i++) {
+        fprintf(fptr, " %s", params[i]);
+    }
+}
